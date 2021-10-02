43fcbfecfd4e5bd187afad2b8a892118202a4910
John Davidson <jhd1013@msn.com>
2021-10-02 13:53:12 -0500

    Archive queries and new retirement_info table

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,4 +1,26 @@
-﻿-- Creating tables for PH-EmployeeDB
+-- Copy the list of employees born 1952-1953 and hired 1985-1988
+-- into a separate table.
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') AND
+	  (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Count the number of employees born 1952-1953 and hired 1985-1988.
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') AND
+	  (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- Add fk from dept_emp to salaries
+ALTER TABLE dept_emp 
+	ADD CONSTRAINT dept_emp_salaries_emp_no FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+
+
+-- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
     dept_no varchar(4)   NOT NULL,
     dept_name varchar(40)  NOT NULL,
@@ -51,5 +73,4 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
 );
